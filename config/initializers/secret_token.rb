cddedb804b286ac0f1632bb58fc8569e7396e0f7
Chad Shaffer <chad.russell.shaffer@gmail.com>
2013-01-19 17:13:50 -0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cherryberry::Application.config.secret_token = 'aeb8b6b9260d23ca33881dce0d0500446365136168499604e83e79b52f414b30fa4d95c188a773ef77429dd946c808e641c45cec64cf19b340b4efba34ebe23d'
