a78b27b1d47e5b309182046a47df7a1608d39098
Chad Shaffer <chad.russell.shaffer@gmail.com>
2013-01-19 21:34:07 -0800

    Adding static pages

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,4 +51,6 @@ Cherryberry::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+  
+  config.serve_static_assets = true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Cherryberry::Application.routes.draw do
-  authenticated :user do
-    root :to => 'home#index'
-  end
-  root :to => "home#index"
-  devise_for :users
-  resources :users
+  #authenticated :user do
+  #  root :to => 'home#index'
+  #end
+  root :to => 'high_voltage/pages#show', :id => 'home'
+  #devise_for :users
+  #resources :users
 end
